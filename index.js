a6a2fe4e5f5a221b5e53664142640924d14a909b
AsifaJahanSafa <155294513+AsifaJahanSafa@users.noreply.github.com>
2024-11-17 12:16:13 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const colors=['red','blue','green','yellow']
-let reverse='';
-for(let color of colors){
-    console.log(color)
+// get friends array using while loop
+const friends=['Elon','Warren','Mark', 'Bill'];
+let i=0;
+while(i<friends.length){
+    console.log(i,":",friends[i]);
+    i++
 }
+
+// reverse friends array using while loop
+let n=0;
+let reverse=[];
+while(n<friends.length){
+    console.log(friends[n]);
+    reverse.unshift(friends[n]);
+   n++
+}
+
+console.log(reverse)
